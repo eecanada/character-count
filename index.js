999bc241e3191c6fde6f23c33a72d26702dc5462
Eder Canada <canada.eder@gmail.com>
2021-11-11 15:20:56 -0800

    mvp

---
+++ index.js
@@ -0,0 +1,21 @@
+function charCount(charLimit) {
+  let warning = document.querySelector('.warning');
+  let inputForm = document.querySelector('.input-form');
+  inputForm.maxLength = `${charLimit}`;
+  let num = document.querySelector('.num');
+  num.textContent = charLimit;
+  let numberCount = Number(num.textContent);
+
+  inputForm.addEventListener('keyup', function (e) {
+    if (inputForm.value.length >= charLimit) {
+      warning.style.display = 'block';
+      console.log(inputForm.value.length);
+    } else {
+      warning.style.display = 'none';
+    }
+
+    num.textContent = numberCount - inputForm.value.length;
+  });
+}
+
+charCount(140);
